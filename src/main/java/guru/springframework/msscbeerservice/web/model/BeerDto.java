17a5d62c3cc4df9fb3020d29c60b598a6e029472
João Marcos Rodrigues <joaomarcos@gipisitemas.com.br>
2021-08-13 09:26:49 -0300

    Bild

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,7 +1,6 @@
 package guru.springframework.msscbeerservice.web.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +11,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+
 public class BeerDto {
     private UUID id;
     private Integer version;
